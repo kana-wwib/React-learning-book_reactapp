18563b5b3de46a446e5241b7cc2af6fba2a8d289
kana-wwib <mukrabwy103@gmail.com>
2020-08-25 12:11:07 +0900

    Completed chapter 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, useDebugValue } from 'react';
+import Rect from './Rect';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let theme = {
+  light:{
+    backgroundColor: "#eef",
+    color: "#006",
+    padding: "10px",
+  },
+  dark:{
+    backgroundColor: "#006",
+    color: "#eef",
+    padding: "10px",
+  }
+};
+
+const ThemeContext = React.createContext(theme.light);
+
+class App extends Component {
+  static contextType = ThemeContext;
+
+  render() {
+    return (
+      <div style={this.context}>
+        <Title value="Content page" />
+        <Message value="This is Content sample." />
+        <Message value="※これはテーマのサンプルです。" />
+      </div>
+    );
+  }
 }
+
+class Title extends Component {
+  static contextType = ThemeContext;
+
+  render() {
+    return (
+      <h2 style={this.context}>{this.props.value}</h2>
+    );
+  }
+}
+
+class Message extends Component {
+    static contextType = ThemeContext;
+  render() {
+    return (
+      <p style={this.context}>{this.props.value}</p>
+    )
+  }
+}
+
 
 export default App;
