5f60752195a3bde282b6c516fd70fbabcfdc968a
kana-wwib <mukrabwy103@gmail.com>
2020-08-25 19:37:38 +0900

    Completed MEMO

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import MemoStore from './memo/Store'
 
+//表示をレンダリング
 ReactDOM.render(
-    <App />, document.getElementById('root')
+    <Provider store={MemoStore}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
+
+
 
